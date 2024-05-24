6fb0d1f3437af27e6e68450babd9d25ff00ea1f9
Nisha0511 <169175323+Nisha0511@users.noreply.github.com>
2024-05-24 21:12:53 +0530

    Update Global.java

--- src/main/java/ProjectE2E/framework/Global.java
+++ src/main/java/ProjectE2E/framework/Global.java
@@ -3,7 +3,7 @@ package ProjectE2E.framework;
 public class Global {
 
 	//windows-we add backward sslash to use any path
-	
+	//abc
 	public static String TestDataPath=System.getProperty("user.dir") + "//TestData//TestData.xlsx";
 	
 	
